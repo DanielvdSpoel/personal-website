ccec88e421ed2c4c0a0668b0921e3e19cd6bcb89
Daniël van der Spoel <contact@danielvdspoel.nl>
2023-08-26 17:48:27 +0200

    Upgrade to laravel 10

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -4,7 +4,6 @@ namespace App\Http;
 
 use App\Http\Middleware\HandleInertiaRequests;
 use App\Http\Middleware\SelectCorrectLanguageMiddleware;
-use Filament\Forms\Components\Select;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -19,7 +18,7 @@ class Kernel extends HttpKernel
     protected $middleware = [
         // \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\TrustProxies::class,
-        \Fruitcake\Cors\HandleCors::class,
+        \Illuminate\Http\Middleware\HandleCors::class,
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
@@ -59,7 +58,7 @@ class Kernel extends HttpKernel
      *
      * @var array
      */
-    protected $routeMiddleware = [
+    protected $middlewareAliases = [
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
