098f0227e48f9fd791def6afcc74603f856eda7b
Daniël van der Spoel <contact@danielvdspoel.nl>
2023-08-26 17:11:17 +0200

    Test

--- app/Http/Middleware/SelectCorrectLanguageMiddleware.php
+++ app/Http/Middleware/SelectCorrectLanguageMiddleware.php
@@ -18,12 +18,19 @@ class SelectCorrectLanguageMiddleware
     public function handle(Request $request, Closure $next)
     {
         $url = Url::fromString($request->url());
-        dd($url->getHost());
 
-        $redirect_domains = explode(',', config('app.redirect_domains'));
-        if (in_array($request->getHost(), $redirect_domains)) {
-            return redirect(route('home', ['language' => 'nl']));
+        if ($url->getHost() !== config('domains.english_domain')) {
+            $language = config('domains.languages.' . $url->getHost());
+            dd($language);
+
+            $redirect_domains = explode(',', config('app.redirect_domains'));
+            if (in_array($request->getHost(), $redirect_domains)) {
+                return redirect(route('home', ['language' => 'nl']));
+            }
         }
+
+
+
         app()->setLocale($request->language);
         return $next($request);
     }
---
+++ config/domains.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'english_domain' => env('ENGLISH_DOMAIN', 'danielvdspoel.com'),
+    'languages' => [
+        'danielvdspoel.nl' => 'nl',
+    ]
+];
