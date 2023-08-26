fd6b57abae6854af346ec2f80179db1f79134d8a
Daniël van der Spoel <contact@danielvdspoel.nl>
2023-08-26 17:32:38 +0200

    Fix redirect issue

--- app/Http/Middleware/SelectCorrectLanguageMiddleware.php
+++ app/Http/Middleware/SelectCorrectLanguageMiddleware.php
@@ -17,14 +17,6 @@ class SelectCorrectLanguageMiddleware
      */
     public function handle(Request $request, Closure $next)
     {
-        //Check if request is to danielvdspoel.com
-
-        //if no, check what tld is used
-
-        //check which languages is used for that tld
-
-        //redirect to danielvdspoel.com with the correct language
-
         if ($request->getHost() !== config('domains.english_domain')) {
             $parts = explode('.', $request->getHost());
             $tld = array_pop($parts);
@@ -38,27 +30,7 @@ class SelectCorrectLanguageMiddleware
                 ->withHost(config('domains.english_domain'))
                 ->withPath(route($request->route()->getName(), ['language' => $lang], false));
             return redirect()->to($newUrl);
-//            dump(route($request->route()->getName(), ['language' => $lang], false), );
-//            dump($lang);
-//            dd($newUrl);
         }
-
-
-
-//        $url = Url::fromString($request->url());
-//
-//        if ($url->getHost() !== config('domains.english_domain')) {
-//            $tld = explode('.', $url->getHost());
-//            $language = config('domains.languages.' . $url->getHost());
-//            dd($language);
-//
-//            $redirect_domains = explode(',', config('app.redirect_domains'));
-//            if (in_array($request->getHost(), $redirect_domains)) {
-//                return redirect(route('home', ['language' => 'nl']));
-//            }
-//        }
-
-
 
         app()->setLocale($request->language);
         return $next($request);
