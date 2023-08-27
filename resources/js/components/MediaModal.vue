e85ec52d6694257266823209285b64d9e5d2ce4e
Daniël van der Spoel <contact@danielvdspoel.nl>
2023-08-27 19:55:24 +0200

    Small changes to project page

--- resources/js/components/MediaModal.vue
+++ resources/js/components/MediaModal.vue
@@ -1,13 +1,13 @@
 <template>
     <TransitionRoot as="template" :show="open">
-        <Dialog as="div" class="relative z-10" @close="$emit('close')">
+        <Dialog as="div" class="relative z-50" @close="$emit('close')">
             <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0" enter-to="opacity-100" leave="ease-in duration-200" leave-from="opacity-100" leave-to="opacity-0">
                 <div class="fixed inset-0 bg-zinc-800/40 backdrop-blur-sm dark:bg-black/80 opacity-100" />
             </TransitionChild>
-            <div class="fixed inset-0 z-10 overflow-y-auto">
+            <div class="fixed inset-0 z-50 overflow-y-auto">
                 <div class="flex justify-center p-4 text-center sm:items-center sm:p-0">
                     <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" enter-to="opacity-100 translate-y-0 sm:scale-100" leave="ease-in duration-200" leave-from="opacity-100 translate-y-0 sm:scale-100" leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
-                        <DialogPanel class="sm:max-w-[50%] relative transform overflow-hidden shadow-xl transition-all rounded-3xl bg-white p-8 ring-1 ring-zinc-900/5 dark:bg-zinc-900 dark:ring-zinc-800 sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
+                        <DialogPanel class="sm:max-w-[50%] relative transform overflow-hidden shadow-xl transition-all rounded-3xl bg-white p-8 ring-1 ring-zinc-900/5 dark:bg-zinc-900 dark:ring-zinc-800 sm:my-8 sm:w-full sm:p-6">
                             <img :alt="media.alt" :src="media.url" class="w-full">
                         </DialogPanel>
                     </TransitionChild>
